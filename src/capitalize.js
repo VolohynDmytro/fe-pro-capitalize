70a02bc1e15dd15f14f3f50a3d584dbb87dd2073
VolohynDmytro <voloshindima777@gmail.com>
2021-12-26 13:54:52 +0200

    first comm

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,10 @@
-function capitalize(string) {
-}
-
-module.exports = capitalize;
+const toCapitalize = string => {
+    const words = string.split(" ");
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ") 
+    }
+    
+    const longstr = "gdfgdf fhdfghjgfj jghfjghf jfhgjfgh";
+    
+    ;
+    console.log(toCapitalize(longstr));
+    module.exports = toCapitalize;
